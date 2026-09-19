4824fcdfac6cf24406971d581ee7f2d2eeaa2c90
unknown <chucv1995@gmail.com>
2026-09-19 13:20:37 +0700

    Init project

---
+++ app/blog/[slug]/page.test.tsx
@@ -0,0 +1,17 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Page from "./page";
+
+it("App Router: Works with dynamic route segments", () => {
+  render(<Page params={{ slug: "Test" }} />);
+  expect(screen.getByRole("heading")).toHaveTextContent("Slug: Test");
+});
+
+it("holetext should be in the documetn",() =>{
+  render(<Page params={{ slug: "Test" }} />);
+  expect(screen.getByText("Holetext")).toBeInTheDocument();
+
+})
