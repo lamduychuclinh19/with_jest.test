2d202a28e46894ea19478c1f6b46bf0b972bbe31
unknown <chucv1995@gmail.com>
2026-09-19 13:27:00 +0700

    Update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -9,5 +9,10 @@ export async function generateMetadata({ params }: Params) {
 }
 
 export default function Page({ params }: Params) {
-  return <><h1>Slug: {params.slug}</h1><p>Holetext</p></>
+  return <>
+  <h1>Slug: {params.slug}</h1>
+  <p>Holetext</p>
+  <p>HoletextMIlk</p>
+  </>
+
 }
